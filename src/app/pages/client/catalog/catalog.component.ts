c7c4a531475695beef9b7c23f9029efd2e036038
Martin Castillo <martin.castillo.francino@alumnos.uta.cl>
2025-11-16 21:47:52 -0300

    Catálogo agregado, comando 'npm start' para correr base de datos json y ng server a la vez

--- src/app/pages/client/catalog/catalog.component.ts
+++ src/app/pages/client/catalog/catalog.component.ts
@@ -1,8 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hotel } from '../../../src/app/core/models/hotel.model';
+import { HotelService } from '../../../src/app/core/services/hotel.service';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.scss'],
 })
-export class CatalogComponent {}
+export class CatalogComponent implements OnInit {
+  hotels: Hotel[] = [];
+  loading = false;
+  error: string | null = null;
+
+  constructor(private hotelService: HotelService) {}
+
+  ngOnInit(): void {
+    this.loadHotels();
+  }
+
+  private loadHotels(): void {
+    this.loading = true;
+    this.error = null;
+
+    this.hotelService.getHotels().subscribe({
+      next: (hotels) => {
+        this.hotels = hotels;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Error cargando hoteles', err);
+        this.error =
+          'No se pudo cargar el catálogo de hoteles. Inténtalo nuevamente.';
+        this.loading = false;
+      },
+    });
+  }
+
+  trackById(index: number, hotel: Hotel): number | string {
+    return hotel.id ?? index;
+  }
+}
